f246050aee84983263e96b863544dfaaed22c21a
Niiok <play.niiok@gmail.com>
2024-12-30 19:42:51 +0900

    initial project

---
+++ Source/ProjectSE/ProjectSEGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectSEGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectSEGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectSEGameMode();
+};
+
+
+
